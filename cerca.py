a50bf4116162501d94753dc3f47bd4db17774d2e
Francesc Lapedriza <franklapedriza@gmail.com>
2017-01-05 20:10:38 +0100

    Molts canvis

--- cerca.py
+++ cerca.py
@@ -1,25 +1,52 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import re
+import time
+import unicodedata
+import xml.etree.ElementTree as Xml
+from datetime import date, datetime, timedelta
 from math import radians, sin, cos, sqrt, asin
 from urllib.request import urlopen
-import xml.etree.ElementTree as XML
-import datetime
-import time
 
 MODE_DEBUG = True
+
 #######################################
 ################ Utils ################
 #######################################
+
+RADI_TERRA = 6367.4447
+
+
+def elimina_accents(s):
+    """
+    Elimina tots els accents de l'entrada
+    :param s: String amb possibles accents
+    :return: String original amb tots els accents eliminats
+    """
+    s = s.replace('ñ', '\u0001')
+    s = s.replace('ç', '\u0002')
+    ret = ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+    ret = ret.replace('\u0001', 'ñ')
+    return ret.replace('\u0002', 'ç')
+
+
 def comptatemps(metode):
+    """
+    Decorador per a calcular el temps emprat en exacutar un mètode
+    """
+
     def temps(*args, **kwargs):
         start = time.time()
         res = metode(*args, **kwargs)
         end = time.time()
         if MODE_DEBUG:
-            print ('{}: {} segons' \
-                .format(metode.__name__, end-start))
+            print('{}: {} segons'
+                  .format(metode.__name__, end - start))
         return res
+
     return temps
+
 
 def haversine(lat, lon, lat2, lon2):
     """
@@ -31,8 +58,6 @@ def haversine(lat, lon, lat2, lon2):
     :param lon2: Longitud del segon punt geogràfic
     :return: Distància
     """
-    RADI_TERRA = 6367.4447
-
     lat, lon, lat2, lon2 = map(radians, [lat, lon, lat2, lon2])
 
     dlat = lat2 - lat
@@ -42,14 +67,13 @@ def haversine(lat, lon, lat2, lon2):
 
     return c * RADI_TERRA
 
-    
 
 def insert(elem, llista, start=None, end=None):
     """
     Inserta una tupla (distància, element) en una llista d'elements ordenada
     per distància.
     :param elem: Element a insertar
-    :param llista: Llista ordenada de tuples (distància, estació)
+    :param llista: Llista ordenada de tuples (distància, element)
     :param start: Paràmetre opcional per a les crides recursives
     :param end: Paràmetre opcional per a les crides recursives
     """
@@ -76,9 +100,8 @@ def insert(elem, llista, start=None, end=None):
     if elem[0] > llista[middle][0]:
         insert(elem, llista, middle + 1, end)
         return
-    
 
-    
+
 #######################################
 ############## ESTACIONS ##############
 #######################################
@@ -90,7 +113,6 @@ class Estacio(object):
     """
     Estació de bicing
     """
-
     attrs = {
         'id': int,
         'street': str,
@@ -123,9 +145,11 @@ class Estacio(object):
 def amb_bicis(estacions):
     return [x for x in estacions if x.disponible() and x.te_bicis()]
 
+
 @comptatemps
 def amb_lloc(estacions):
     return [x for x in estacions if x.disponible() and x.te_llocs()]
+
 
 @comptatemps
 def bicing_a_prop(estacions, lat, lon, dist=0.5):
@@ -139,18 +163,19 @@ def bicing_a_prop(estacions, lat, lon, dist=0.5):
 
     return [y for (x, y) in ret]
 
+
 @comptatemps
 def get_estacions(url=URL_BICING):
     """
     Obté una llista amb totes les dades de les estacions de bicing del web
     service de l'ajuntament de Barcelona.
-    :param url: URL del servei de l'ajuntament de Barcelona
+    :param url: URL del servei de l'ajuntament de Barcelona (opcional)
     :return: Llista amb les dades de totes les estacions de Bicing de Barcelona
     """
     ret = []
     with urlopen(url) as openurl:
         xml = openurl.read()
-    reader = XML.fromstring(xml)
+    reader = Xml.fromstring(xml)
     for elem in reader.findall('station'):
         e = Estacio()
         for k, v in Estacio.attrs.items():
@@ -165,6 +190,8 @@ def get_estacions(url=URL_BICING):
 #######################################
 
 URL_APARCAMENTS = 'http://www.bcn.cat/tercerlloc/Aparcaments.xml'
+
+RUTA_APARCAMENTS = 'search/queryresponse/list/list_items/row'
 
 
 class Aparcament(object):
@@ -182,6 +209,7 @@ class Aparcament(object):
     def distancia(self, lat, lon):
         return haversine(self.gmapx, self.gmapy, lat, lon)
 
+
 @comptatemps
 def aparcaments_a_prop(aparcaments, lat, lon, d=0.5):
     ret = []
@@ -192,13 +220,20 @@ def aparcaments_a_prop(aparcaments, lat, lon, d=0.5):
 
     return [y for (x, y) in ret]
 
+
 @comptatemps
 def get_aparcaments(url=URL_APARCAMENTS):
+    """
+    Obté una llista amb totes les dades dels aparcaments del web
+    service de l'ajuntament de Barcelona.
+    :param url: URL del servei de l'ajuntament de Barcelona (opcional)
+    :return: Llista amb les dades de tots els aparcaments de Barcelona
+    """
     ret = []
     with urlopen(url) as openurl:
         xml = openurl.read()
-    reader = XML.fromstring(xml)
-    parkings = reader.findall('search/queryresponse/list/list_items/row')
+    reader = Xml.fromstring(xml)
+    parkings = reader.findall(RUTA_APARCAMENTS)
     for park in parkings:
         p = Aparcament()
         for k, v in Aparcament.attrs.items():
@@ -206,28 +241,141 @@ def get_aparcaments(url=URL_APARCAMENTS):
             p.__setattr__(k, v(text))
         ret.append(p)
     return ret
-    
+
+
 #######################################
 ############ ESDEVENIMENTS ############
 #######################################
 
 URL_ESDEVENIMENTS = 'http://www.bcn.cat/tercerlloc/agenda_cultural.xml'
 
+INICI_INFO_ESDEVENIMENT = 'http://guia.barcelona.cat/ca/detall/'
+
 FORMAT_DATA = '%d/%m/%Y'
 
-RUTA_ROWS = 'search/queryresponse/list/list_items/row'
+RUTA_ESDEVENIMENTS = 'search/queryresponse/list/list_items/row'
 
-to_date = lambda x: datetime.datetime.strptime(x, FORMAT_DATA).date()
+STRING_FORM = re.compile(r'[\"\']?\w+[\s\w*]*[\"\']?')
+
+
+def to_date(x): return datetime.strptime(x, FORMAT_DATA).date()
+
 
 class Esdeveniment(object):
     def __init__(self, xmlel):
         xmlel = xmlel.find('item')
+        self.url_mes_info = INICI_INFO_ESDEVENIMENT + xmlel.items()[0][1]
         self.nom = xmlel.find('name').text
-        self.adreça = xmlel.find('address').text
-        interes = xmlel.find('interestinfo/item')
-        if interes:
-            self.info_interes = interes.find('label').text + ': ' + \
-                interes.find('interinfo')
+        try:
+            self.adreça = xmlel.find('address').text
+        except AttributeError:
+            self.adreça = 'N/D'
+        try:
+            self.barri = xmlel.find('addresses/item/barri').text
+        except AttributeError:
+            self.barri = 'N/D'
+        try:
+            self.lat = float(xmlel.find('gmapx').text)
+            self.lon = float(xmlel.find('gmapy').text)
+        except AttributeError:
+            self.lat = self.lon = float(0.0)
+        try:
+            self.lloc = xmlel.find('institutionname').text
+        except AttributeError:
+            self.lloc = 'N/D'
+        try:
+            interes = xmlel.find('interestinfo/item')
+            self.info_interes = '{}: {}'.format(interes.find('label').text,
+                                                interes.find('interinfo').text)
+        except AttributeError:
+            self.info_interes = 'N/D'
+        data_inici = xmlel.find('begindate')
+        if data_inici is not None:
+            self.data_inici = to_date(data_inici.text)
+            self.data_fi = to_date(xmlel.find('enddate').text)
         else:
-            self.info_interes = None
-        
+            data = xmlel.find('date').text
+            if data == 'Acte Permanent':
+                self.data_inici = date.min
+                self.data_fi = date.max
+            else:
+                self.data_inici = self.data_fi = \
+                    to_date(xmlel.find('date').text)
+
+    @comptatemps
+    def aparcaments_propers(self, aparcaments):
+        """
+        Retorna una llista amb tots els aparcaments a menys de 500m del event
+        :param aparcaments: Llista d'aparcaments
+        :return: @description
+        """
+        return aparcaments_a_prop(aparcaments, self.lat, self.lon)
+
+    @comptatemps
+    def bicing_propers(self, estacions):
+        """
+        Retorna una tupla que conté una llista amb les estacions de bicing
+        amb bicicletes disponibles i les estacions de bicing amb llocs
+        disponibles respectivament que es troben a menys de 500m de l'event
+        :param estacions: Llista d'estacions de bicing
+        :return: @description
+        """
+        estacions = bicing_a_prop(estacions, self.lat, self.lon)
+        return amb_bicis(estacions), amb_lloc(estacions)
+
+
+@comptatemps
+def get_esdeveniments(url=URL_ESDEVENIMENTS):
+    """
+    Obté una llista amb totes les dades dels esdeveniments de la ciutat del web
+    service de l'ajuntament de Barcelona.
+    :param url: URL del servei de l'ajuntament de Barcelona (opcional)
+    :return: Llista amb les dades de tots els esdeveniments de Barcelona
+    durant l'any actual.
+    """
+    ret = set()
+    with urlopen(url) as openurl:
+        xml = openurl.read()
+    reader = Xml.fromstring(xml)
+    esdeveniments = reader.findall(RUTA_ESDEVENIMENTS)
+    for ev in esdeveniments:
+        e = Esdeveniment(ev)
+        ret.add(e)
+    return ret
+
+
+@comptatemps
+def busca_esdeveniments(query, esdeveniments):
+    try:
+        if STRING_FORM.match(query) is None:
+            quey = eval(query)
+    except:
+        pass
+
+    if isinstance(query, tuple):
+        ret = set()
+        for elem in query:
+            ret = ret.union(busca_esdeveniments(elem,
+                                                esdeveniments.difference(ret)))
+        return ret
+    elif isinstance(query, list):
+        ret = esdeveniments
+        for elem in query:
+            ret = busca_esdeveniments(elem, ret)
+        return ret
+    elif isinstance(query, str):
+        ret = set()
+        for elem in esdeveniments:
+            cerca = elimina_accents(query.lower())
+            font = elimina_accents("{}|{}|{}".format(elem.nom, elem.lloc,
+                                                     elem.barri).lower())
+            if cerca in font:
+                ret.add(elem)
+        return ret
+
+
+# TODO
+def esdeveniments_periode(esdeveniments, data, marge):
+    ret = set()
+    for esd in esdeveniments:
+        pass
--- prova.py
+++ prova.py
@@ -7,14 +7,10 @@ lon = 2.1684163
 
 ests = get_estacions()
 parks = get_aparcaments()
+esdev = get_esdeveniments()
 
 park_prop = aparcaments_a_prop(parks, lat, lon)
 
 est_prop = bicing_a_prop(ests, lat, lon)
 
-print('Num. aparcaments {}'.format(len(park_prop)))
-print('Num. estacionss {}'.format(len(est_prop)))
 
-print(park_prop[0])
-
-print(est_prop[0])
